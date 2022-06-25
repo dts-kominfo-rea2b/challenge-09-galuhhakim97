3c9bb8272c5c6cc14a9430e0a8da860f32ab9221
Galuh <galuhhakim97@gmail.com>
2022-06-25 12:46:09 +0700

    App.js Fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import contacts from './data/contacts.json';
+import Contacts from './components/Contact';
+import Header from './components/Header';
+
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
@@ -7,7 +11,15 @@ const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+        <div className='bingkai'>
+          <Contacts data={contacts[0]}/>
+          <Contacts data={contacts[1]}/>
+          <Contacts data={contacts[2]}/>
+          <Contacts data={contacts[3]}/>
+          <Contacts data={contacts[4]}/>
+          <Contacts data={contacts[5]}/>
+        </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+     <div>
+        <h1 className='judul'>Call A Friend</h1>
+        <h3 className='sub-judul'>"your friendly contact app"</h3>
+    </div>
     )
 }
 
